93fe99b42968ba6e9d940dc5d8ad08c98af7ea9e
julidsa <julianadesa830@gmail.com>
2023-04-13 16:46:08 -0300

    tenzies game with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import Die from "./Die.js"
+import {nanoid} from "nanoid"
+import Confetti from 'react-confetti'
+import { useTimer } from 'use-timer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const {time, start, pause, reset} = useTimer()
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+    const [count, setCount] = React.useState(0)
 
-export default App;
+    const [attempt, setAttempt] = React.useState(
+        JSON.parse(localStorage.getItem('attempt') || null
+    ))
+    const [bestTime, setBestTime ] = React.useState(
+       JSON.parse(localStorage.getItem('bestTime')) || null)
+
+    React.useEffect(() => {
+        localStorage.setItem('attempt', attempt)
+    }, [attempt])
+
+    React.useEffect(() => {
+        localStorage.setItem('bestTime', bestTime)
+    }, [bestTime])
+
+    React.useEffect(() => {
+        const allHelds = dice.every(die => die.isHeld) 
+        const firstValue = dice[0].value
+        const allValue = dice.every(die => die.value === firstValue)
+         
+        if (allHelds && allValue) {
+            setTenzies(true)
+            pause()
+            
+            if (attempt > count) {
+                setAttempt(count)
+            }
+
+            if(time < bestTime) {
+                setBestTime(time)
+            }
+        }
+    }, [dice])
+    
+    function generateNewDice() {
+        return {
+            value: Math.ceil(Math.random() * 6), 
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    
+    function allNewDice() {
+       const arr = []
+        
+       for (let i = 0; i < 10; i++) {
+          arr.push(generateNewDice())
+        }
+         return arr
+    }    
+        
+    function rollDice() {
+        if (!tenzies) {
+            setCount(count + 1)
+            setDice(prev => 
+                prev.map((dice) => {
+                    return dice.isHeld ? 
+                    dice : generateNewDice()
+                }))
+        } else {
+            setTenzies(false)
+            setDice(allNewDice())
+            setCount(0)
+            reset()
+        }
+    }
+    
+    function holdDice(id) {
+        start()
+        setDice(prev =>
+            prev.map((dice) => {
+                return dice.id === id ? {...dice, isHeld: !dice.isHeld} : dice
+            })
+    )}
+        
+    const diceElements = dice.map(die => (
+         <Die 
+            key={die.id} 
+            value={die.value}
+            isHeld={die.isHeld}
+            holdDice={() => holdDice(die.id)}
+        />
+))         
+    return (
+        <main>
+            {tenzies && <Confetti /> }
+
+            <h1>{tenzies ? "You won! 🥳" : "Tenzies"}</h1>
+
+            <p>Roll until all dice are the same. 
+            Click each die to freeze it at its current 
+            value between rolls.</p>
+            
+            <p className="record">
+                🏆 <b>{attempt} </b>
+                - ⏱️ <b>{bestTime}s </b>
+            </p>
+
+            <div className="dice-container" >
+                {diceElements}
+            </div>
+            
+            <div className='buttons'>
+            <button className="button-roll" onClick={rollDice}> {tenzies ? "New Game" : "Roll"}</button>
+            <p className="number-roll" >{count} rolls </p>
+            </div>
+
+            {tenzies && <hr />}
+            
+            {tenzies &&
+                <div className='result'>
+                    <p className='p-result'><b>{count}</b> tentativas </p>
+                    <p className='p-result'>⏱️ <b>{time}s</b></p>
+                </div>
+            }
+            
+        </main>
+    )
+  }
+ 
+  /** */
